90a22a50dd727875df1a30c824f0d3cc9500f566
Jonas Moeller <jonas.moeller@mindmatters.de>
2016-01-30 18:12:13 +0100

    Dashboard annoyers output

--- app/helpers/annoyers_helper.rb
+++ app/helpers/annoyers_helper.rb
@@ -1,2 +1,21 @@
 module AnnoyersHelper
+  def get_amount_of_nodes annoyer
+    nodes = Node.where annoyer_id: annoyer.id
+    nodes.count
+  end
+  def get_latest_recent annoyer
+    reminders = Reminder.where annoyer_id: annoyer.id
+    latest_recent = Date.new
+    latest_reminder = reminders.last
+    
+    reminders.each do |reminder|
+      recent_tmp = reminder.recents.order("created_at").last
+      if latest_recent < recent_tmp.created_at
+        latest_recent = recent_tmp.created_at
+        latest_reminder = reminder
+      end
+    end
+
+    {date: latest_recent.to_formatted_s(:long_ordinal), recent: latest_reminder.title}
+  end
 end
