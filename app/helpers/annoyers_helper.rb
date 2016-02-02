7b685431aa23072bbe6c23d830c7ed694b92b3c0
Jonas Moeller <jonas.moeller@mindmatters.de>
2016-02-02 17:28:18 +0100

    add 10 latest recents

--- app/helpers/annoyers_helper.rb
+++ app/helpers/annoyers_helper.rb
@@ -3,11 +3,12 @@ module AnnoyersHelper
     nodes = Node.where annoyer_id: annoyer.id
     nodes.count
   end
+
   def get_latest_recent annoyer
     reminders = Reminder.where annoyer_id: annoyer.id
     latest_recent = Date.new
     latest_reminder = reminders.last
-    
+
     reminders.each do |reminder|
       recent_tmp = reminder.recents.order("created_at").last
       if latest_recent < recent_tmp.created_at
@@ -17,5 +18,25 @@ module AnnoyersHelper
     end
 
     {date: latest_recent.to_formatted_s(:long_ordinal), recent: latest_reminder.title}
+  end
+
+  def get_overall_latest_recents annoyers
+    all_reminders = Reminder.where annoyer_id: annoyers.map(&:id)
+    all_recents = Recent.where reminder_id: all_reminders.map(&:id)
+    recents = all_recents.order("created_at desc").limit("10")
+
+    recent_reminder_arr = Array.new
+    recents.each do |recent|
+      recents_reminder = Reminder.where(id: recent.reminder_id).first
+      recent_reminder_arr.push(
+        {
+          date: recent.created_at.to_formatted_s(:long_ordinal),
+          reminder: recents_reminder.title,
+          color: Annoyer.where(id: recents_reminder.annoyer_id).first.color
+        }
+      )
+    end
+
+    return recent_reminder_arr
   end
 end
