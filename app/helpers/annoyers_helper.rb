e9e11b8d8f9433c69f8e7c6799bf790ae7950920
Jonas Moeller <jonas.moeller@mindmatters.de>
2016-02-03 17:48:12 +0100

    fix stylingissues and refactoring

--- app/helpers/annoyers_helper.rb
+++ app/helpers/annoyers_helper.rb
@@ -17,18 +17,20 @@ module AnnoyersHelper
     all_recents = Recent.where reminder_id: reminder_ids
     recents = all_recents.order("created_at desc").limit("10")
 
-    recent_reminder_arr = Array.new
-    recents.each do |recent|
-      recents_reminder = Reminder.where(id: recent.reminder_id).first
-      recent_reminder_arr.push(
-        {
-          date: recent.created_at.to_formatted_s(:long_ordinal),
-          reminder: recents_reminder.title,
-          color: Annoyer.where(id: recents_reminder.annoyer_id).first.color
-        }
-      )
-    end
-
-    return recent_reminder_arr
+    map_recent_reminder_data recents
   end
+
+  private
+    def map_recent_reminder_data recents
+      recent_reminder_arr = Array.new
+      recents.each do |recent|
+        recents_reminder = Reminder.where(id: recent.reminder_id).first
+        recent_reminder_arr.push({
+            date: recent.created_at.to_formatted_s(:long_ordinal),
+            reminder: recents_reminder.title,
+            color: Annoyer.where(id: recents_reminder.annoyer_id).first.color
+        })
+      end
+      return recent_reminder_arr
+    end
 end
