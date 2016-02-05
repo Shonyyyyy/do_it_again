7cca0220cc0df0566fad0bcaa2acbf6a48defb46
Jonas Moeller <jonas.moeller@mindmatters.de>
2016-02-05 11:13:05 +0100

    refacorting for session validation in controller

--- app/controllers/annoyers_controller.rb
+++ app/controllers/annoyers_controller.rb
@@ -1,66 +1,62 @@
 class AnnoyersController < ApplicationController
   def index
-    if current_user
+    session_exists do
       @annoyers = Annoyer.all
-    else
-      redirect_to root_path
     end
   end
 
   def new
-    if current_user
+    session_exists do
       @annoyer = Annoyer.new
-    else
-      redirect_to root_path
     end
   end
 
   def create
-    if current_user
+    session_exists do
       @annoyer = Annoyer.new annoyer_params
       if @annoyer.save
         redirect_to @annoyer
       else
         render 'new'
       end
-    else
-      redirect_to root_path
     end
   end
 
   def show
-    if current_user
+    session_exists do
       @annoyer = Annoyer.find params[:id]
       @node = Node.new
       @reminder = Reminder.new
-    else
-      redirect_to root_path
     end
   end
 
   def edit
-    if current_user
+    session_exists do
       @annoyer = Annoyer.find params[:id]
-    else
-      redirect_to root_path
     end
   end
 
   def update
-    if current_user
+    session_exists do
       @annoyer = Annoyer.find params[:id]
       if @annoyer.update annoyer_params
         redirect_to @annoyer
       else
         redirect_to edit_annoyer_path(@annoyer)
       end
-    else
-      redirect_to root_path
     end
   end
 
   private
     def annoyer_params
       params.require(:annoyer).permit(:title, :color)
+    end
+
+    def session_exists
+      if current_user
+        yield
+      else
+        redirect_to root_path
+      end
     end
 end
